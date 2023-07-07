41bd9b06463157d842958a9c3d88c62b49391ba3
MollyToxic <natalazdanova14887@gmail.com>
2023-07-07 09:54:53 +0300

    data table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => setPosts(json));
+  }, [])
+  function deletePost(id){
+    const newPosts = posts.filter((post)=> id!==post.id)
+
+    setPosts(newPosts)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My first application
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table border="1" width="1200px" height="100px">
+          <thead>
+            <tr>
+              <td>userId</td>
+              <td>id</td>
+              <td>title</td>
+              <td>body</td>
+              <td>actions</td>
+            </tr>
+          </thead>
+          <tbody>
+            {posts.map(post =>
+              <tr key={post.id}>
+                <td>{post.userId}</td>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+                <td><button onClick={()=> deletePost(post.id)}>D</button></td>
+              </tr>)}
+
+          </tbody>
+
+        </table>
       </header>
     </div>
   );
 }
+
 
 export default App;
